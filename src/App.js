8c1e45b4899410287b8b83a182830618d5d14dcb
khalifa5000 <m.khalifa5000@gmail.com>
2024-04-30 00:08:00 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import { useRef, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const inputRef = useRef();
+
+  const handleAddTodo = () => {
+    const text = inputRef.current.value;
+    const newItem = { completed: false, text };
+    setTodos([...todos, newItem]);
+    inputRef.current.value = "";
+  };
+
+  const handleItemDone = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  const handleDeleteItem = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
+  }
+
+  return (
+    <div className="App">
+      <h2>To Do List</h2>
+      <div className="to-do-container">
+        <ul>
+          {todos.map(({ text, completed }, index) => {
+            return (
+              <div className="item">
+                <li
+                  className={completed ? "done" : ""}
+                  key={index}
+                  onClick={() => handleItemDone(index)}
+                >
+                  {text}
+                </li>
+                <span onClick={() => handleDeleteItem(index)} className="trash">❌</span>
+              </div>
+            );
+          })}
+        </ul>
+        <input ref={inputRef} placeholder="Enter item..." />
+        <button onClick={handleAddTodo}>Add</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
